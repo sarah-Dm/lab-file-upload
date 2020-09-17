0b93158f1b95388d4d6f44525e997272712207d4
Sarah Damag <sarahdamag@gmail.com>
2020-09-17 21:05:56 +0200

    iteration 1 done

--- models/User.model.js
+++ models/User.model.js
@@ -8,7 +8,7 @@ const userSchema = new Schema(
       type: String,
       trim: true,
       required: [true, 'Username is required.'],
-      unique: true
+      unique: true,
     },
     email: {
       type: String,
@@ -17,15 +17,16 @@ const userSchema = new Schema(
       match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
     passwordHash: {
       type: String,
-      required: [true, 'Password is required.']
-    }
+      required: [true, 'Password is required.'],
+    },
+    imageURL: String,
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
 
